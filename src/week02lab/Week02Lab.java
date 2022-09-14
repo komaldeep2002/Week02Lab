ac2cacfead8f8bb722f987598181f702da153481
Komaldeep Kaur Virk <Komaldeep Kaur Virk@DESKTOP-0GGC8NH>
2022-09-13 22:45:03 -0600

    lab2 code added

--- src/week02lab/Week02Lab.java
+++ src/week02lab/Week02Lab.java
@@ -17,7 +17,7 @@ public class Week02Lab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Lab2");
+       System.out.println("Week2Lab2");
     }
     
 }
